90106ae9ba86296eba6aad76461ce7d24e00b171
Harshitha-R31 <harshitharamachandra31@gmail.com>
2026-06-04 16:06:21 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,23 +1,12 @@
 import pandas as pd
 import streamlit as st
-import os
 
 @st.cache_data
 def load_data():
 
-    file_path = "data/ny_real_estate_sold_properties_2026.csv"
-
-    st.write("Looking for:", file_path)
-
-    st.write("File exists:", os.path.exists(file_path))
-
-    st.write(
-        "File size:",
-        os.path.getsize(file_path)
-        if os.path.exists(file_path)
-        else "Not Found"
+    df = pd.read_csv(
+        "data/ny_real_estate_sold_properties_2026.csv",
+        low_memory=False
     )
-
-    df = pd.read_csv(file_path)
 
     return df
